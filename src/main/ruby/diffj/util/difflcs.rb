78af42df6f3003d8e765f9997e14fbd616ffa84c
Jeff Pace <jeugenepace@gmail.com>
2012-04-20 08:58:34 -0400

    More DiffLCS migration to JRuby.

--- src/main/ruby/diffj/util/difflcs.rb
+++ src/main/ruby/diffj/util/difflcs.rb
@@ -58,6 +58,72 @@ module DiffJ
       end
     end
 
+    class Thresholds < org.incava.ijdk.util.diff.Thresholds
+      include Loggable
+      
+      def initialize
+        @map = Hash.new
+      end
+
+      # returns whether the value for the given index is greater than val.
+      def greater_than? idx, val
+        curr = get(idx)
+        curr && val && curr > val
+      end
+
+      # returns whether the value for the given index is less than val.
+      def less_than? idx, val
+        # we're using nil instead of having the elements in the array
+        # initialized to zero.
+        curr = get(idx)
+        curr && (val.nil? || curr < val)
+      end
+
+      # Inserts the given values into the threshold map.
+      def jruby_insert j, k
+        info "j: #{j}; k: #{k}".on_red
+        if k && k != 0 && (j && k > j) && isLessThan(k.intValue() - 1, j)
+          put k, j
+          return k
+        end
+        
+        hi = -1
+        info "hi: #{hi}".red
+            
+        if k && k != 0
+          hi = k
+        elsif !isEmpty()
+          hi = lastKey()
+        end
+
+        # off the end?
+        if hi == -1 || j > getLastValue()
+          append j.to_java(:int)
+          return hi + 1
+        end
+                
+        # binary search for insertion point:
+        lo = 0
+
+        while lo <= hi
+          index = (hi + lo) / 2
+          val   = get(index.to_java :int)
+          cmp   = j <=> val
+
+          if cmp == 0
+            return nil
+          elsif cmp > 0
+            lo = index + 1
+          else
+            hi = index - 1
+          end
+        end
+
+        put lo.to_java(:int), j.to_java(:int)
+        return lo
+      end
+    end
+
     class Comparator < org.incava.ijdk.util.diff.Diff
       include Loggable
 
@@ -68,44 +134,34 @@ module DiffJ
         @to = to
         @diffs = Array.new
         @pending = nil
-        @thresholds = Hash.new
         @comparator = nil       # java.util.Comparator
       end
 
       def lcs
         comp_get_longest_common_subsequences
       end
-
-      def are_equal? x, y
-        # comparator == null ? x.equals(y) : comparator.compare(x, y) == 0
-        x == y
-      end
       
       def comp_get_longest_common_subsequences
-        a = @from
-        b = @to
-
         aStart = 0
-        aEnd = a.size() - 1
+        aEnd = @from.size - 1
 
         bStart = 0
-        bEnd = b.size() - 1
+        bEnd = @to.size - 1
 
         matches = java.util.TreeMap.new
         
-        while aStart <= aEnd && bStart <= bEnd && are_equal?(a[aStart], b[bStart])
+        while aStart <= aEnd && bStart <= bEnd && @from[aStart] == @to[bStart]
           matches.put aStart, bStart
           aStart += 1
           bStart += 1
         end
 
-        while aStart <= aEnd && bStart <= bEnd && are_equal?(a[aEnd], b[bEnd])
+        while aStart <= aEnd && bStart <= bEnd && @from[aEnd] == @to[bEnd]
           matches.put aEnd, bEnd
           aEnd -= 1
           bEnd -= 1
         end
 
-        # addMatches matches, aStart, aEnd, bStart, bEnd
         comp_add_matches matches, aStart, aEnd, bStart, bEnd
 
         to_array matches
@@ -151,7 +207,7 @@ module DiffJ
 
         links = LCSTable.new
 
-        thresh = org.incava.ijdk.util.diff::Thresholds.new()
+        thresholds = Thresholds.new
 
         (aStart .. aEnd).each do |i|
           info "i: #{i}".on_red
@@ -162,8 +218,8 @@ module DiffJ
             k = 0
             pit = positions.listIterator(positions.size())
             while pit.hasPrevious()
-              j = pit.previous();
-              k = thresh.insert(j, k);
+              j = pit.previous()
+              k = thresholds.jruby_insert(j, k)
               if k
                 links.update(i, j, k);
               end
@@ -173,8 +229,8 @@ module DiffJ
 
         info "matches: #{matches}".yellow
 
-        if !thresh.isEmpty()
-          ti = thresh.lastKey();
+        if !thresholds.isEmpty()
+          ti = thresholds.lastKey();
           chain = links.get_chain ti
           info "chain: #{chain}".yellow
           chain.each do |key, value|
@@ -187,8 +243,7 @@ module DiffJ
       end
       
       def to_array map
-        size = map.isEmpty() ? 0 : 1 + map.lastKey()
-        ary = Array.new
+        ary = Array.new(map.isEmpty() ? 0 : 1 + map.lastKey())
         map.keySet().each do |key|
           ary[key] = map.get(key)
         end
--- src/test/ruby/diffj/util/test_difflcs.rb
+++ src/test/ruby/diffj/util/test_difflcs.rb
@@ -24,7 +24,7 @@ class DiffJ::DiffLCSTestCase < DiffJ::TestCase
     end
   end
 
-  def run_lcs_test from, to
+  def run_lcs_test from, to, exp = nil
     info "from: #{from}"
     info "to: #{to}"
 
@@ -35,6 +35,7 @@ class DiffJ::DiffLCSTestCase < DiffJ::TestCase
     cmp = Comparator.new from, to
     jrubyresult = cmp.lcs
 
+    info "jrubyresult: #{jrubyresult.inspect}".yellow
     assert_equal javaresult.size, jrubyresult.size
 
     (0 ... javaresult.size).each do |idx|
@@ -42,41 +43,73 @@ class DiffJ::DiffLCSTestCase < DiffJ::TestCase
       info "jrubyresult[#{idx}]: #{jrubyresult[idx].inspect}".cyan
       assert_equal javaresult[idx], jrubyresult[idx], "idx: #{idx}"
     end
+
+    if exp
+      info "exp: #{exp.inspect}".yellow
+      assert_equal exp.size, jrubyresult.size
+      
+      (0 ... exp.size).each do |idx|
+        info "exp        [#{idx}]: #{exp[idx].inspect}".yellow
+        info "jrubyresult[#{idx}]: #{jrubyresult[idx].inspect}".cyan
+        assert_equal exp[idx], jrubyresult[idx], "idx: #{idx}"
+      end
+    end
   end
 
   def test_jruby_lcs_abcd_cd
     from = %w{ a b c d }
     to   = %w{     c d }
 
-    run_lcs_test from, to
+    exp = [ nil, nil, 0, 1 ]
+    run_lcs_test from, to, exp
   end
 
   def test_jruby_lcs_abcd_ab
     from = %w{ a b c d }
     to   = %w{ a b }
 
-    run_lcs_test from, to
+    exp = [ 0, 1 ]
+    run_lcs_test from, to, exp
   end
 
   def test_jruby_lcs_abcd_abcd
     from = %w{ a b c d }
     to   = %w{ a b c d }
 
-    run_lcs_test from, to
+    exp = [ 0, 1, 2, 3 ]
+    run_lcs_test from, to, exp
   end
 
   def test_jruby_lcs_ad_abcd
     from = %w{ a d }
     to   = %w{ a b c d }
 
-    run_lcs_test from, to
+    exp = [ 0, 3 ]
+    run_lcs_test from, to, exp
   end
 
   def test_jruby_lcs_abcdefgh_abefh
     from = %w{ a b c d e f g h }
     to   = %w{ a b     e f   h }
     
-    run_lcs_test from, to
+    exp = [ 0, 1, nil, nil, 2, 3, nil, 4 ]
+    run_lcs_test from, to, exp
+  end
+
+  def test_jruby_lcs_abcehjlmnp_bcdefjklmrst
+    from = %w{ a b c   e   h j   l m n p }
+    to   = %w{   b c d e f   j k l m     r s t }
+
+    exp = [ nil, 0, 1, 3, nil, 5, 7, 8 ]
+    run_lcs_test from, to, exp
+  end    
+
+  def test_jruby_lcs_aaaabbbaaaabbbaaaabbbaaaabbb_aaaabbbabbbaaaa
+    from = %w{ a a a a b b b a a a a b b b a a a a b b b a a a a b b b }
+    to   = %w{ a a a a b b b a       b b b a a a a }
+
+    exp = [ 0, 1, 2, 3, 4, 5, 6, 7, nil, nil, nil, 8, 9, 10, 11, 12, 13, 14 ]
+    run_lcs_test from, to, exp
   end
 
   def xxxtest_java_deleted_two
