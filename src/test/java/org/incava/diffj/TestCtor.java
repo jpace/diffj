06da4aca477a84727a4ce54a81340eadf3b4ed52
Jeff Pace <jeugenepace@gmail.com>
2012-10-13 16:51:40 -0400

    Split tests into subsets.

--- src/test/java/org/incava/diffj/TestCtor.java
+++ src/test/java/org/incava/diffj/TestCtor.java
@@ -1,8 +1,6 @@
 package org.incava.diffj;
 
 import java.text.MessageFormat;
-import org.incava.analysis.FileDiff;
-import org.incava.analysis.FileDiffChange;
 import org.incava.diffj.io.JavaFile;
 import org.incava.ijdk.text.Location;
 import org.incava.java.Java;
@@ -10,111 +8,6 @@ import org.incava.java.Java;
 public class TestCtor extends ItemsTest {
     public TestCtor(String name) {
         super(name);
-    }
-
-    public void testCodeNotChanged() {
-        evaluate(new Lines("class Test {",
-                           "    Test() { i = -1; }",
-                           "",
-                           "}"),
-
-                 new Lines("class Test {",
-                           "",
-                           "    Test() { ",
-                           "        i = -1;",
-                           "    }",
-                           "",
-                           "}"),
-                 
-                 NO_CHANGES);
-    }
-
-    public void testCodeChanged() {
-        evaluate(new Lines("class Test {",
-                           "    Test() { int i = -1; }",
-                           "",
-                           "}"),
-
-                 new Lines("class Test {",
-                           "",
-                           "    Test() { ",
-                           "        int i = -2;",
-                           "    }",
-                           "}"),
-                 
-                 makeCodeChangedRef(Messages.CODE_CHANGED, "Test()", loc(2, 23), loc(2, 23), loc(4, 18), loc(4, 18)));
-    }
-    
-    public void testCodeInsertedSameLine() {
-        evaluate(new Lines("class Test {",
-                           "    Test() { int i = -1; }",
-                           "",
-                           "}"),
-
-                 new Lines("class Test {",
-                           "",
-                           "    Test() { ",
-                           "        int j = 0;",
-                           "        int i = -1;",
-                           "    }",
-                           "}"),
-                 
-                 makeCodeAddedRef(Messages.CODE_ADDED, "Test()", loc(2, 18), loc(2, 18), loc(4, 13), loc(5, 11)));
-    }
-
-    public void testCodeAddedOwnLine() {
-        evaluate(new Lines("class Test {",
-                           "    Test() { ",
-                           "        char ch;",
-                           "        int i = -1;",
-                           "    }",
-                           "",
-                           "}"),
-                 
-                 new Lines("class Test {",
-                           "",
-                           "    Test() { ",
-                           "        char ch;",
-                           "        if (true) { }",
-                           "        int i = -1;",
-                           "    }",
-                           "}"),
-                 
-                 makeCodeAddedRef(Messages.CODE_ADDED, "Test()", loc(4, 9), loc(4, 11), loc(5, 9), loc(5, 21)));
-    }
-
-    public void testCodeDeleted() {
-        evaluate(new Lines("class Test {",
-                           "    Test() { ",
-                           "        int j = 0;",
-                           "        int i = -1;",
-                           "    }",
-                           "",
-                           "}"),
-
-                 new Lines("class Test {",
-                           "",
-                           "    Test() { int i = -1; }",
-                           "}"),
-                 
-                 makeCodeDeletedRef(Messages.CODE_REMOVED, "Test()", loc(3, 13), loc(4, 11), loc(3, 18), loc(3, 18)));
-    }
-    
-    public void testCodeInsertedAndChanged() {
-        evaluate(new Lines("class Test {",
-                           "    Test(int i) { i = 1; }",
-                           "",
-                           "}"),
-
-                 new Lines("class Test {",
-                           "",
-                           "    Test(int i) { ",
-                           "        int j = 0;",
-                           "        i = 2;",
-                           "    }",
-                           "}"),
-
-                 makeCodeChangedRef(Messages.CODE_CHANGED, "Test(int)", loc(2, 19), loc(2, 23), loc(4,  9), loc(5, 13)));
     }
 
     public void testCtorWithParameterizedTypeDump() {
@@ -170,19 +63,5 @@ public class TestCtor extends ItemsTest {
                            "}"),
                  Java.SOURCE_1_6,
                  NO_CHANGES);
-    }
-
-    protected String paramReordMsg(String paramName, int oldPosition, int newPosition) {
-        return MessageFormat.format(Messages.PARAMETER_REORDERED, paramName, Integer.valueOf(oldPosition), Integer.valueOf(newPosition));
-    }
-
-    protected String paramReordRenamedMsg(String oldName, int oldPosition, String newName, int newPosition) {
-        return MessageFormat.format(Messages.PARAMETER_REORDERED_AND_RENAMED, oldName, Integer.valueOf(oldPosition), Integer.valueOf(newPosition), newName);
-    }
-
-    protected FileDiff makeChangedRef(String msg, 
-                                      String fromStr, String toStr,
-                                      Location fromStart, Location toStart) {
-        return new FileDiffChange(getMessage(null, null, msg, fromStr, toStr), fromStart, loc(fromStart, fromStr), toStart, loc(toStart, toStr));
     }
 }
---
+++ src/test/java/org/incava/diffj/TestCtorCode.java
@@ -0,0 +1,114 @@
+package org.incava.diffj;
+
+import java.text.MessageFormat;
+
+public class TestCtorCode extends ItemsTest {
+    public TestCtorCode(String name) {
+        super(name);
+    }
+
+    public void testCodeNotChanged() {
+        evaluate(new Lines("class Test {",
+                           "    Test() { i = -1; }",
+                           "",
+                           "}"),
+
+                 new Lines("class Test {",
+                           "",
+                           "    Test() { ",
+                           "        i = -1;",
+                           "    }",
+                           "",
+                           "}"),
+                 
+                 NO_CHANGES);
+    }
+
+    public void testCodeChanged() {
+        evaluate(new Lines("class Test {",
+                           "    Test() { int i = -1; }",
+                           "",
+                           "}"),
+
+                 new Lines("class Test {",
+                           "",
+                           "    Test() { ",
+                           "        int i = -2;",
+                           "    }",
+                           "}"),
+                 
+                 makeCodeChangedRef(Messages.CODE_CHANGED, "Test()", loc(2, 23), loc(2, 23), loc(4, 18), loc(4, 18)));
+    }
+    
+    public void testCodeInsertedSameLine() {
+        evaluate(new Lines("class Test {",
+                           "    Test() { int i = -1; }",
+                           "",
+                           "}"),
+
+                 new Lines("class Test {",
+                           "",
+                           "    Test() { ",
+                           "        int j = 0;",
+                           "        int i = -1;",
+                           "    }",
+                           "}"),
+                 
+                 makeCodeAddedRef(Messages.CODE_ADDED, "Test()", loc(2, 18), loc(2, 18), loc(4, 13), loc(5, 11)));
+    }
+
+    public void testCodeAddedOwnLine() {
+        evaluate(new Lines("class Test {",
+                           "    Test() { ",
+                           "        char ch;",
+                           "        int i = -1;",
+                           "    }",
+                           "",
+                           "}"),
+                 
+                 new Lines("class Test {",
+                           "",
+                           "    Test() { ",
+                           "        char ch;",
+                           "        if (true) { }",
+                           "        int i = -1;",
+                           "    }",
+                           "}"),
+                 
+                 makeCodeAddedRef(Messages.CODE_ADDED, "Test()", loc(4, 9), loc(4, 11), loc(5, 9), loc(5, 21)));
+    }
+
+    public void testCodeDeleted() {
+        evaluate(new Lines("class Test {",
+                           "    Test() { ",
+                           "        int j = 0;",
+                           "        int i = -1;",
+                           "    }",
+                           "",
+                           "}"),
+
+                 new Lines("class Test {",
+                           "",
+                           "    Test() { int i = -1; }",
+                           "}"),
+                 
+                 makeCodeDeletedRef(Messages.CODE_REMOVED, "Test()", loc(3, 13), loc(4, 11), loc(3, 18), loc(3, 18)));
+    }
+    
+    public void testCodeInsertedAndChanged() {
+        evaluate(new Lines("class Test {",
+                           "    Test(int i) { i = 1; }",
+                           "",
+                           "}"),
+
+                 new Lines("class Test {",
+                           "",
+                           "    Test(int i) { ",
+                           "        int j = 0;",
+                           "        i = 2;",
+                           "    }",
+                           "}"),
+
+                 makeCodeChangedRef(Messages.CODE_CHANGED, "Test(int)", loc(2, 19), loc(2, 23), loc(4,  9), loc(5, 13)));
+    }
+}
