8058760354de11d9076ac3bea103eaf2bf52a302
Jeff Pace <jeugenepace@gmail.com>
2012-10-14 18:42:12 -0400

    Cleaned up supers (extends, implements) code.

---
+++ src/main/java/org/incava/diffj/type/NameToDecl.java
@@ -0,0 +1,51 @@
+package org.incava.diffj.type;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
+import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
+import net.sourceforge.pmd.ast.SimpleNode;
+import org.incava.diffj.Differences;
+import org.incava.pmdx.SimpleNodeUtil;
+
+/**
+ * The list of supertypes for 'extends' and 'implements'.
+ */
+public class NameToDecl {
+    private final Map<String, ASTClassOrInterfaceType> map;
+
+    public NameToDecl(ASTClassOrInterfaceDeclaration decl, String extImpClassName) {
+        this.map = new HashMap<String, ASTClassOrInterfaceType>();
+        SimpleNode list = SimpleNodeUtil.findChild(decl, extImpClassName);
+
+        if (list == null) {
+            return;
+        }
+        
+        Collection<ASTClassOrInterfaceType> types = new ArrayList<ASTClassOrInterfaceType>();
+        SimpleNodeUtil.fetchChildren(types, list, "net.sourceforge.pmd.ast.ASTClassOrInterfaceType");
+        for (ASTClassOrInterfaceType type : types) {
+            map.put(SimpleNodeUtil.toString(type), type);
+        }
+    }
+    
+    public boolean hasOne() {
+        return map.size() == 1;
+    }
+
+    public ASTClassOrInterfaceType get(String name) {
+        return map.get(name);
+    }
+
+    public Collection<String> getNames() {
+        return new TreeSet<String>(map.keySet());
+    }
+
+    protected String getFirstName() {
+        return map.keySet().iterator().next();
+    }
+}
--- src/main/java/org/incava/diffj/type/Supers.java
+++ src/main/java/org/incava/diffj/type/Supers.java
@@ -1,15 +1,9 @@
 package org.incava.diffj.type;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceType;
-import net.sourceforge.pmd.ast.SimpleNode;
 import org.incava.diffj.Differences;
-import org.incava.pmdx.SimpleNodeUtil;
 
 /**
  * Compares super (extends or implements).
@@ -40,67 +34,54 @@ public abstract class Supers {
     abstract protected String getChangedMessage();
 
     abstract protected String getRemovedMessage();
-
-    protected <K, V> K getFirstKey(Map<K, V> map) {
-        return map.keySet().iterator().next();
-    }
     
     public void diff(Supers toSupers, Differences differences) {
         ASTClassOrInterfaceDeclaration toDecl = toSupers.decl;
 
-        Map<String, ASTClassOrInterfaceType> fromMap = getMap();
-        Map<String, ASTClassOrInterfaceType> toMap = toSupers.getMap();
+        NameToDecl fromMap = getMap();
+        NameToDecl toMap = toSupers.getMap();
 
-        // I don't like this special case, but it is better than two separate
-        // "add" and "remove" messages.
-
-        if (fromMap.size() == 1 && toMap.size() == 1) {
-            String fromName = getFirstKey(fromMap);
-            String toName = getFirstKey(toMap);
-
-            if (!fromName.equals(toName)) {
-                ASTClassOrInterfaceType fromType = fromMap.get(fromName);
-                ASTClassOrInterfaceType toType = toMap.get(toName);
-                
-                superTypeChanged(fromType, fromName, toType, toName, differences);
-            }
+        if (fromMap.hasOne() && toMap.hasOne()) {
+            // I don't like this special case, but it is better than two separate
+            // "add" and "remove" messages.
+            compareOne(fromMap, toMap, differences);
         }
         else {
-            List<String> typeNames = new ArrayList<String>();
-            typeNames.addAll(fromMap.keySet());
-            typeNames.addAll(toMap.keySet());
+            compareEach(fromMap, toMap, toSupers, differences);
+        }
+    }
 
-            // tr.Ace.log("typeNames", typeNames);
+    protected void compareOne(NameToDecl fromMap, NameToDecl toMap, Differences differences) {
+        String fromName = fromMap.getFirstName();
+        String toName = toMap.getFirstName();
 
-            for (String typeName : typeNames) {
-                ASTClassOrInterfaceType fromType = fromMap.get(typeName);
-                ASTClassOrInterfaceType toType = toMap.get(typeName);
+        if (!fromName.equals(toName)) {
+            ASTClassOrInterfaceType fromType = fromMap.get(fromName);
+            ASTClassOrInterfaceType toType = toMap.get(toName);
+                
+            superTypeChanged(fromType, fromName, toType, toName, differences);
+        }
+    }
 
-                if (fromType == null) {
-                    superTypeAdded(toType, typeName, differences);
-                }
-                else if (toType == null) {
-                    toSupers.superTypeRemoved(fromType, typeName, differences);
-                }
+    protected void compareEach(NameToDecl fromMap, NameToDecl toMap, Supers toSupers, Differences differences) {
+        Collection<String> names = fromMap.getNames();
+        names.addAll(toMap.getNames());
+
+        for (String name : names) {
+            ASTClassOrInterfaceType fromType = fromMap.get(name);
+            ASTClassOrInterfaceType toType = toMap.get(name);
+
+            if (fromType == null) {
+                superTypeAdded(toType, name, differences);
+            }
+            else if (toType == null) {
+                toSupers.superTypeRemoved(fromType, name, differences);
             }
         }
     }
 
-    protected Map<String, ASTClassOrInterfaceType> getMap() {
+    protected NameToDecl getMap() {
         String extImpClassName = getAstClassName();
-        Map<String, ASTClassOrInterfaceType> map = new HashMap<String, ASTClassOrInterfaceType>();
-        SimpleNode list = SimpleNodeUtil.findChild(decl, extImpClassName);
-
-        if (list == null) {
-            return map;
-        }
-
-        Collection<ASTClassOrInterfaceType> types = new ArrayList<ASTClassOrInterfaceType>();
-        SimpleNodeUtil.fetchChildren(types, list, "net.sourceforge.pmd.ast.ASTClassOrInterfaceType");
-        for (ASTClassOrInterfaceType type : types) {
-            map.put(SimpleNodeUtil.toString(type), type);
-        }
-        
-        return map;
+        return new NameToDecl(decl, extImpClassName);
     }
 }
