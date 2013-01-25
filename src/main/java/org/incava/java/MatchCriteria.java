c4b80c77b9e884e699d5a0c067cb02ed5ab9092c
Jeff Pace <jeugenepace@gmail.com>
2013-01-24 19:28:24 -0500

    Fixed compilation errors.

---
+++ src/main/java/org/incava/java/MatchCriteria.java
@@ -0,0 +1,10 @@
+package org.incava.java;
+
+/**
+ * A criterion (some criteria) for matching nodes.
+ */
+public class MatchCriteria {
+    public double compare(MatchCriteria other) {
+        return 0.0;
+    }
+}
