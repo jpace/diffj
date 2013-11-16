4c28297eaf201406d43fda3b33a2990c2062369e
Jeff Pace <jeugenepace@gmail.com>
2013-11-16 18:34:11 -0500

    Added comparison of statements as strings/tokens.

--- src/test/java/org/incava/diffj/function/TestMethodCodeByStatement.java
+++ src/test/java/org/incava/diffj/function/TestMethodCodeByStatement.java
@@ -13,6 +13,7 @@ import org.incava.diffj.ItemsTest;
 import org.incava.diffj.Lines;
 import org.incava.diffj.io.JavaFile;
 import org.incava.ijdk.text.Location;
+import org.incava.ijdk.util.diff.*;
 import org.incava.java.Java;
 import org.incava.pmdx.SimpleNodeUtil;
 import static org.incava.diffj.code.Code.*;
@@ -38,7 +39,7 @@ public class TestMethodCodeByStatement extends ItemsTest {
         SimpleNodeUtil.dump(node);
     }
 
-    public List<Token> dumpTokens(SimpleNode node) {
+    public String tokensToString(SimpleNode node) {
         List<Token> tokens = SimpleNodeUtil.getChildTokens(node);
         tr.Ace.log("tokens", tokens);
         StringBuffer sb = new StringBuffer();
@@ -46,6 +47,13 @@ public class TestMethodCodeByStatement extends ItemsTest {
             sb.append("^").append(tk.image);
         }
         tr.Ace.cyan("sb", sb);
+        return sb.toString();
+    }
+
+    public List<Token> dumpTokens(SimpleNode node) {
+        List<Token> tokens = SimpleNodeUtil.getChildTokens(node);
+        tr.Ace.log("tokens", tokens);
+        String str = tokensToString(node);
         return tokens;
     }
 
@@ -53,36 +61,55 @@ public class TestMethodCodeByStatement extends ItemsTest {
         return SimpleNodeUtil.findChild(parent, null, idx);
     }
 
-    public void show(String fileName) throws Exception {
+    public ASTCompilationUnit getCompilationUnit(String fileName) throws Exception {
         URL url = seek(fileName);
         File file = new File(url.toURI());
-
         JavaFile javaFile = new JavaFile(file, fileName, "1.5");
-        
-        ASTCompilationUnit ast = javaFile.compile();
+        return javaFile.compile();
+    }
 
+    /**
+     * Returns the first method. Assumes type->class->body->method.
+     */
+    public SimpleNode getFirstMethod(ASTCompilationUnit ast) {
         SimpleNode typeDecl = getChildNode(ast, 0);
         SimpleNode clsDecl = getChildNode(typeDecl, 0);
         SimpleNode body = getChildNode(clsDecl, 0);
 
         SimpleNode bodyDecl = getChildNode(body, 0);
-        SimpleNode meth = getChildNode(bodyDecl, 0);
+        return getChildNode(bodyDecl, 0);
+    }
+
+    public List<SimpleNode> getStatements(SimpleNode node) {
+        return SimpleNodeUtil.findChildren(node);
+    }
+
+    public List<String> show(String fileName) throws Exception {
+        ASTCompilationUnit ast = getCompilationUnit(fileName);
+        SimpleNode meth = getFirstMethod(ast);
 
         SimpleNode methBlk = getChildNode(meth, 2);
         dump(methBlk);
 
-        List<SimpleNode> statements = SimpleNodeUtil.findChildren(methBlk);
+        List<String> strs = new ArrayList<String>();
+
+        List<SimpleNode> statements = getStatements(methBlk);
         for (SimpleNode stmt : statements) {
             tr.Ace.yellow("stmt", stmt);
             dumpTokens(stmt);
+            strs.add(tokensToString(stmt));
         }
+        return strs;
     }
 
     public void testSomething() throws Exception {
         tr.Ace.setVerbose(true);
         tr.Ace.log("this", this);
         
-        show("diffj/codecomp/d0/Changed.java");
-        show("diffj/codecomp/d1/Changed.java");
+        List<String> a = show("diffj/codecomp/d0/Changed.java");
+        List<String> b = show("diffj/codecomp/d1/Changed.java");
+        Diff<String> diff = new Diff<String>(a, b);
+        List<Difference> diffs = diff.execute();
+        tr.Ace.log("diffs", diffs);
     }
 }
