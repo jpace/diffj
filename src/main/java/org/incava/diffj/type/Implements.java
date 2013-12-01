595aceae6817d4da6ca13d31c4328f0c21f788dc
Jeff Pace <jeugenepace@gmail.com>
2013-11-30 19:47:08 -0500

    Further integration of new Messages class.

--- src/main/java/org/incava/diffj/type/Implements.java
+++ src/main/java/org/incava/diffj/type/Implements.java
@@ -3,6 +3,7 @@ package org.incava.diffj.type;
 import net.sourceforge.pmd.ast.ASTClassOrInterfaceDeclaration;
 import net.sourceforge.pmd.ast.SimpleNode;
 import org.incava.ijdk.text.Message;
+import org.incava.diffj.util.Messages;
 
 /**
  * Compares implements.
@@ -11,6 +12,7 @@ public class Implements extends Supers {
     public static final Message IMPLEMENTED_TYPE_REMOVED = new Message("implemented type removed: {0}");
     public static final Message IMPLEMENTED_TYPE_ADDED = new Message("implemented type added: {0}");
     public static final Message IMPLEMENTED_TYPE_CHANGED = new Message("implemented type changed from {0} to {1}");
+    public static final Messages IMPLEMENTED_TYPE_MSGS = new Messages(IMPLEMENTED_TYPE_ADDED, IMPLEMENTED_TYPE_CHANGED, IMPLEMENTED_TYPE_REMOVED);
 
     public Implements(ASTClassOrInterfaceDeclaration decl) {
         super(decl);
@@ -21,14 +23,14 @@ public class Implements extends Supers {
     }
 
     protected Message getAddedMessage() {
-        return IMPLEMENTED_TYPE_ADDED;
+        return IMPLEMENTED_TYPE_MSGS.getAdded();
     }
 
     protected Message getChangedMessage() {
-        return IMPLEMENTED_TYPE_CHANGED;
+        return IMPLEMENTED_TYPE_MSGS.getChanged();
     }
 
     protected Message getRemovedMessage() {
-        return IMPLEMENTED_TYPE_REMOVED;
+        return IMPLEMENTED_TYPE_MSGS.getDeleted();
     }
 }
