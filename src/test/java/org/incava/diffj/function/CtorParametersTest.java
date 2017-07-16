ae7a277e198c40edb63d38f45e1e19d730fe98e3
Jeff Pace <jeugenepace@gmail.com>
2017-07-16 12:14:09 -0400

    Clean up test of ctor and method parameters comparisons.

--- src/test/java/org/incava/diffj/function/CtorParametersTest.java
+++ src/test/java/org/incava/diffj/function/CtorParametersTest.java
@@ -1,42 +1,35 @@
 package org.incava.diffj.function;
 
+import org.incava.diffj.util.Lines;
+
 public class CtorParametersTest extends FunctionTestCase {
     public CtorParametersTest(String name) {
         super(name);
     }
 
+    private Lines testCode(String params) {
+        return lines("class Test {",
+                     "    Test(" + params + ") {}",
+                     "}");
+    }
+    
     public void testParameterAddedNoneToOne() {
-        evaluate(lines("class Test {",
-                       "    Test() {}",
-                       "}"),
-                 
-                 lines("class Test {",
-                       "    Test(Integer i) {}",
-                       "}"),
+        evaluate(testCode(""),
+                 testCode("Integer i"),
 
                  paramAdded("i", locrg(2, 9, 10), locrg(2, 18, 18)));
     }
 
     public void testParameterAddedOneToTwo() {
-        evaluate(lines("class Test {",
-                       "    Test(String s) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(String s, Integer i) {}",
-                       "}"),
+        evaluate(testCode("String s"),
+                 testCode("String s, Integer i"),
                  
                  paramAdded("i", locrg(2, 9, 18), locrg(2, 20, 28)));
     }
 
     public void testParameterAddedOneToThree() {
-        evaluate(lines("class Test {",
-                       "    Test(String s) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(List[] ary, String s, Integer i) {}",
-                       "}"),
+        evaluate(testCode("String s"),
+                 testCode("List[] ary, String s, Integer i"),
                  
                  paramAdded("ary",         locrg(2, 9, 18),  locrg(2, 10, 19)),
                  paramAdded("i",           locrg(2, 9, 18),  locrg(2, 32, 40)),
@@ -44,37 +37,22 @@ public class CtorParametersTest extends FunctionTestCase {
     }
 
     public void testParameterRemovedOneToNone() {
-        evaluate(lines("class Test {",
-                       "    Test(Integer i[][][][]) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test() {}",
-                       "}"),
+        evaluate(testCode("Integer i[][][][]"),
+                 testCode(""),
                  
                  paramRemoved("i", locrg(2, 18, 18), locrg(2, 9, 10)));
     }
 
     public void testParameterRemovedTwoToOne() {
-        evaluate(lines("class Test {",
-                       "    Test(String s, Integer i) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(String s) {}",
-                       "}"),
+        evaluate(testCode("String s, Integer i"),
+                 testCode("String s"),
                  
                  paramRemoved("i", locrg(2, 20, 28), locrg(2, 9, 18)));
     }
 
     public void testParameterRemovedThreeToOne() {
-        evaluate(lines("class Test {",
-                       "    Test(List[] ary, String s, Integer i) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(String s) {}",
-                       "}"),
+        evaluate(testCode("List[] ary, String s, Integer i"),
+                 testCode("String s"),
                  
                  paramRemoved("ary",       locrg(2, 10, 19), locrg(2, 9, 18)),
                  paramReordered("s", 1, 0, locrg(2, 29, 29), locrg(2, 17, 17)),
@@ -82,63 +60,38 @@ public class CtorParametersTest extends FunctionTestCase {
     }
 
     public void testParameterChangedType() {
-        evaluate(lines("class Test {",
-                       "    Test(int i) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(Integer i) {}",
-                       "}"),
+        evaluate(testCode("int i"),
+                 testCode("Integer i"),
                  
                  paramTypeChanged("int", "Integer", locrg(2, 10, 14), locrg(2, 10, 18)));
     }
 
     public void testParameterChangedName() {
-        evaluate(lines("class Test {",
-                       "    Test(int i) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(int x) {}",
-                       "}"),
+        evaluate(testCode("int i"),
+                 testCode("int x"),
 
                  paramNameChanged("i", "x", locrg(2, 14, 14), locrg(2, 14, 14)));
     }
 
     public void testParameterReordered() {
-        evaluate(lines("class Test {",
-                       "    Test(int i, double d) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(double d, int i) {}",
-                       "}"),
+        evaluate(testCode("int i, double d"),
+                 testCode("double d, int i"),
                  
                  paramReordered("i", 0, 1, locrg(2, 14, 14), locrg(2, 24, 24)),
                  paramReordered("d", 1, 0, locrg(2, 24, 24), locrg(2, 17, 17)));
     }
 
     public void testParameterReorderedAndRenamed() {
-        evaluate(lines("class Test {",
-                       "    Test(int i, double d) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(double dbl, int i2) {}",
-                       "}"),
+        evaluate(testCode("int i, double d"),
+                 testCode("double dbl, int i2"),
                  
                  paramReorderedAndRenamed("i", 0, 1, "i2",  locrg(2, 14, 14), locrg(2, 26, 27)),
                  paramReorderedAndRenamed("d", 1, 0, "dbl", locrg(2, 24, 24), locrg(2, 17, 19)));
     }
 
     public void testParameterOneAddedOneReordered() {
-        evaluate(lines("class Test {",
-                       "    Test(int i) {}",
-                       "}"),
-
-                 lines("class Test {",
-                       "    Test(int i2, int i) {}",
-                       "}"),
+        evaluate(testCode("int i"),
+                 testCode("int i2, int i"),
                  
                  paramAdded("i2",          locrg(2, 9, 15),  locrg(2, 10, 15)),
                  paramReordered("i", 0, 1, locrg(2, 14, 14), locrg(2, 22, 22)));
--- src/test/java/org/incava/diffj/function/MethodParametersTest.java
+++ src/test/java/org/incava/diffj/function/MethodParametersTest.java
@@ -1,169 +1,98 @@
 package org.incava.diffj.function;
 
+import org.incava.diffj.util.Lines;
+
 public class MethodParametersTest extends FunctionTestCase {
     public MethodParametersTest(String name) {
         super(name);
     }
 
+    private Lines testCode(String params) {
+        return lines("class Test {",
+                     "    void abc(" + params + ") {}",
+                     "}");
+    }
+    
     public void testParameterAddedNoneToOne() {
-        evaluate(lines("class Test {",
-                       "    void foo() {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "",
-                       "    void foo(Integer i) {}",
-                       "}"),
-
-                 paramAdded("i", locrg(2, 13, 14), locrg(3, 22, 22)));
+        evaluate(testCode(""),
+                 testCode("Integer i"),
+                 paramAdded("i", locrg(2, 13, 14), locrg(2, 22, 22)));
     }
 
     public void testParameterAddedOneToTwo() {
-        evaluate(lines("class Test {",
-                       "    void foo(String s) {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "",
-                       "    void foo(String s, Integer i) {}",
-                       "}"),
-
-                 paramAdded("i", locrg(2, 13, 22), locrg(3, 24, 32)));
+        evaluate(testCode("String s"),
+                 testCode("String s, Integer i"),
+                 paramAdded("i", locrg(2, 13, 22), locrg(2, 24, 32)));
     }
 
     public void testParameterAddedOneToThree() {
-        evaluate(lines("class Test {",
-                       "    void foo(String s) {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "",
-                       "    void foo(List[] ary, String s, Integer i) {}",
-                       "}"),
+        evaluate(testCode("String s"),
+                 testCode("List[] ary, String s, Integer i"),
                  
-                 paramAdded("ary", locrg(2, 13, 22), locrg(3, 14, 23)),
-                 paramAdded("i",   locrg(2, 13, 22), locrg(3, 36, 44)),
-                 paramReordered("s", 0, 1, locrg(2, 21, 21), locrg(3, 33, 33)));
+                 paramAdded("ary", locrg(2, 13, 22), locrg(2, 14, 23)),
+                 paramAdded("i",   locrg(2, 13, 22), locrg(2, 36, 44)),
+                 paramReordered("s", 0, 1, locrg(2, 21, 21), locrg(2, 33, 33)));
     }
 
     public void testParameterRemovedOneToNone() {
-        evaluate(lines("class Test {",
-                       "    void foo(Integer i[][][][]) {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "",
-                       "    void foo() {}",
-                       "}"),
+        evaluate(testCode("Integer i[][][][]"),
+                 testCode(""),
                  
-                 paramRemoved("i", locrg(2, 22, 22), locrg(3, 13, 14)));
+                 paramRemoved("i", locrg(2, 22, 22), locrg(2, 13, 14)));
     }
 
     public void testParameterRemovedTwoToOne() {
-        evaluate(lines("class Test {",
-                       "    void foo(String s, Integer i) {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "    void foo(String s) {}",
-                       "",
-                       "}"),
+        evaluate(testCode("String s, Integer i"),
+                 testCode("String s"),
                  
                  paramRemoved("i", locrg(2, 24, 32), locrg(2, 13, 22)));
     }
 
     public void testParameterRemovedThreeToOne() {
-        evaluate(lines("class Test {",
-                       "    void foo(List[] ary, String s, Integer i) {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "    void foo(String s) {}",
-                       "",
-                       "}"),
+        evaluate(testCode("List[] ary, String s, Integer i"),
+                 testCode("String s"),
                  
-                 paramRemoved("ary", locrg(2, 14, 23), locrg(2, 13, 22)),
+                 paramRemoved("ary",       locrg(2, 14, 23), locrg(2, 13, 22)),
                  paramReordered("s", 1, 0, locrg(2, 33, 33), locrg(2, 21, 21)),
-                 paramRemoved("i", locrg(2, 36, 44), locrg(2, 13, 22)));
+                 paramRemoved("i",         locrg(2, 36, 44), locrg(2, 13, 22)));
     }
 
     public void testParameterChangedType() {
-        evaluate(lines("class Test {",
-                       "    void foo(int i) {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "",
-                       "    void foo(Integer i) {}",
-                       "}"),
+        evaluate(testCode("int i"),
+                 testCode("Integer i"),
                  
-                 paramTypeChanged("int", "Integer", locrg(2, 14, 18), locrg(3, 14, 22)));
+                 paramTypeChanged("int", "Integer", locrg(2, 14, 18), locrg(2, 14, 22)));
     }
 
     public void testParameterChangedName() {
-        evaluate(lines("class Test {",
-                       "    void foo(int i) {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "",
-                       "    void foo(int x) {}",
-                       "}"),
+        evaluate(testCode("int i"),
+                 testCode("int x"),
                  
-                 paramNameChanged("i", "x", locrg(2, 18, 18), locrg(3, 18, 18)));
+                 paramNameChanged("i", "x", locrg(2, 18, 18), locrg(2, 18, 18)));
     }
 
     public void testParameterReordered() {
-        evaluate(lines("class Test {",
-                       "    void foo(int i, double d) {}",
-                       "",
-                       "}"),
-
-                 lines("class Test {",
-                       "",
-                       "    void foo(double d, int i) {}",
-                       "}"),
+        evaluate(testCode("int i, double d"),
+                 testCode("double d, int i"),
                  
-                 paramReordered("i", 0, 1, locrg(2, 18, 18), locrg(3, 28, 28)),
-                 paramReordered("d", 1, 0, locrg(2, 28, 28), locrg(3, 21, 21)));
+                 paramReordered("i", 0, 1, locrg(2, 18, 18), locrg(2, 28, 28)),
+                 paramReordered("d", 1, 0, locrg(2, 28, 28), locrg(2, 21, 21)));
     }
 
     public void testParameterReorderedAndRenamed() {
-        evaluate(lines("class Test {",
-                       "    void foo(int i, double d) {}",
-                       "",
-                       "}"),
+        evaluate(testCode("int i, double d"),
+                 testCode("double dbl, int i2"),
 
-                 lines("class Test {",
-                       "",
-                       "    void foo(double dbl, int i2) {}",
-                       "}"),
-
-                 paramReorderedAndRenamed("i", 0, 1, "i2", locrg(2, 18, 18), locrg(3, 30, 31)),
-                 paramReorderedAndRenamed("d", 1, 0, "dbl", locrg(2, 28, 28), locrg(3, 21, 23)));
+                 paramReorderedAndRenamed("i", 0, 1, "i2",  locrg(2, 18, 18), locrg(2, 30, 31)),
+                 paramReorderedAndRenamed("d", 1, 0, "dbl", locrg(2, 28, 28), locrg(2, 21, 23)));
     }
 
     public void testParameterOneAddedOneReordered() {
-        evaluate(lines("class Test {",
-                       "    void foo(int i) {}",
-                       "",
-                       "}"),
+        evaluate(testCode("int i"),
+                 testCode("int i2, int i"),
 
-                 lines("class Test {",
-                       "",
-                       "    void foo(int i2, int i) {}",
-                       "}"),
-
-                 paramAdded("i2", locrg(2, 13, 19), locrg(3, 14, 19)),
-                 paramReordered("i", 0, 1, locrg(2, 18, 18), locrg(3, 26, 26)));
+                 paramAdded("i2",          locrg(2, 13, 19), locrg(2, 14, 19)),
+                 paramReordered("i", 0, 1, locrg(2, 18, 18), locrg(2, 26, 26)));
     }
 
     public void testParameterReorderedByName() {
@@ -179,12 +108,12 @@ public class MethodParametersTest extends FunctionTestCase {
                        "                    String string1, String string2, String string3, java.lang.Object[] obj1, java.lang.Object[] obj2);",
                        "}"),
                  
-                 paramAdded("obj", locrg(2, 39, 4, 28), locrg(3, 21, 40)),
-                 paramAdded("string1", locrg(2, 39, 4, 28), locrg(4, 21, 34)),
-                 paramAdded("string2", locrg(2, 39, 4, 28), locrg(4, 37, 50)),
-                 paramAdded("string3", locrg(2, 39, 4, 28), locrg(4, 53, 66)),
-                 paramRemoved("ctx", locrg(2, 40, 56), locrg(3, 20, 4, 117)),
-                 paramReordered("obj1", 1, 4, locrg(3, 9, 27), locrg(4, 69, 91)),
-                 paramReordered("obj2", 2, 5, locrg(4, 9, 27), locrg(4, 94, 116)));
+                 paramAdded("obj",            locrg(2, 39, 4, 28), locrg(3, 21, 40)),
+                 paramAdded("string1",        locrg(2, 39, 4, 28), locrg(4, 21, 34)),
+                 paramAdded("string2",        locrg(2, 39, 4, 28), locrg(4, 37, 50)),
+                 paramAdded("string3",        locrg(2, 39, 4, 28), locrg(4, 53, 66)),
+                 paramRemoved("ctx",          locrg(2, 40, 56),    locrg(3, 20, 4, 117)),
+                 paramReordered("obj1", 1, 4, locrg(3, 9, 27),     locrg(4, 69, 91)),
+                 paramReordered("obj2", 2, 5, locrg(4, 9, 27),     locrg(4, 94, 116)));
     }
 }
